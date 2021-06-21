78342caff0629c13eb7c62cb8737fb608998084d
FinalProyectWeb <finalproyectpw@gmail.com>
2021-06-21 00:20:11 -0500

    Actualizacion de proyecto

---
+++ js/init.js
@@ -0,0 +1,11 @@
+/**Conectamos nuestra aplicación web con
+ * Firebase con las siguientes lineas de código*/
+firebase.initializeApp({
+    apiKey: "AIzaSyCEFLvYOrCyy419tkEnmT_-8Gy3QPtJMeQ",
+    authDomain: "final-proyect-programation-web.firebaseapp.com",
+    projectId: "final-proyect-programation-web",
+    storageBucket: "final-proyect-programation-web.appspot.com",
+    messagingSenderId: "1000636705212",
+    appId: "1:1000636705212:web:87270f50bbfc7a75c023b9",
+    measurementId: "G-NPQEN86LP9"
+    });    
